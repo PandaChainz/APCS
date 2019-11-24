a328e81c86ca3cb8251d82d976628a242bf3b343
PandaChainz <tembelisa@bxscience.edu>
2019-11-24 16:46:32 +0000

    Edits to the response methods.

--- Chatbot.java
+++ Chatbot.java
@@ -1,18 +1,18 @@
 public class Chatbot {
-  private int age;
-  private String name;
-  private String place;
-  private String username;
+  private static int age;
+  private static String name;
+  private static String place;
+  private static String username;
   // More will be added
 
   public Chatbot() {
     age = 16;
     name = "Alex";
-    place = "USA";
+    place = "United States of America";
   }
 
   public String greeting() {
-    return (" Hello! What is your name?");
+    return "Hello! I'm Alex. What is your name?";
   }
 
   public void setUsername(String n) {
@@ -23,20 +23,49 @@ public class Chatbot {
     return username;
   }
 
-  public String whatResponse(String phrase) {
+   public static String whatResponse(String phrase) {
     if (phrase.contains("name")) {
       return name;
-    } else {
-      return ("I don't know.");
     }
     if (phrase.contains("age")) {
-      return String.valueOf(age);
-    } else {
+      return "I'm " + Integer.toString(age);
+    }
       return ("I don't know.");
+    }
+
+  public String response(String phrase) {
+    if ((phrase.contains("What")) || (phrase.contains("what"))) {
+      return whatResponse(phrase);
+    } else if ((phrase.contains("Where")) || (phrase.contains("where"))) 
+    {
+      return whereResponse(phrase);
+      } else if ((phrase.contains("How")) || (phrase.contains("how"))) 
+      {
+      return howResponse(phrase);
+      }
+    return "Nice to meet you " + username;
+  }
+
+ 
+
+  public String howResponse(String phrase) {
+    if (phrase.contains("old")) {
+      return "I'm " + Integer.toString(age);
+    } else {
+      return "I am not sure.";
     }
   }
 
-  public String response(String phrase) {
-    return ("hi");
+  public String whereResponse(String phrase) {
+    if ((phrase.contains("from")) || (phrase.contains("live"))) {
+      return "I'm from the " + place;
+    } else {
+      return "I am not sure.";
+    }
   }
+
+  
+  
+  
+  
 }
